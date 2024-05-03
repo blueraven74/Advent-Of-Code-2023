9ed0c7946b89190df2c3cdf3f70d3545d6535347
blueraven74 <122903329+blueraven74@users.noreply.github.com>
2024-05-03 18:59:27 +0530

    Update main.py

--- day1/main.py
+++ day1/main.py
@@ -1,1 +1,19 @@
+nums = '1234567890'
+sum = 0
+num = 0
+with open ('/workspaces/Advent-Of-Code-2023/day1/input.txt', 'r') as f:
+    l = f.readlines()
 
+for i in l:
+    start = None
+    end = None 
+    for j in i:
+        if j in nums and start == None:
+            start = j
+        elif j in nums and start != None:
+            end = j
+    if end == None:
+        end = start
+    num = int(start+end)
+    sum += num
+print(sum)
